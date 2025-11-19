4cae22ee48ee488e0f92391967eddb6394a4a480
dumpjin <harvieunknown@gmail.com>
2025-11-19 23:26:54 +0800

    workplsthanks

--- t3bernardo/src/server/db/index.ts
+++ t3bernardo/src/server/db/index.ts
@@ -12,7 +12,6 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
-
-export const db = drizzle(conn, { schema });
+// const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+// if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+// export const db = drizzle(conn, { schema });
